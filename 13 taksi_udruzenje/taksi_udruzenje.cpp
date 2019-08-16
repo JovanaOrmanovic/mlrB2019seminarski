63d6d13b5b7966f35bcfb9fdfbde2835b09bfec7
JovanaOrmanovic <jovana.ormanovic@gmail.com>
2019-08-16 20:49:58 +0200

    zavrsila

--- 13 taksi_udruzenje/taksi_udruzenje.cpp
+++ 13 taksi_udruzenje/taksi_udruzenje.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int taksisti_vreme[100] = {0};
-  int n;
-  int i = 0;
-  while (cin >> n) {
-    taksisti_vreme[i] = n;
-    i++;
+  int brMin,Min;
+  int brPotencijalnogMin,minDoDolaska;
+  cin>>brMin>>Min;
+  while (cin>>brPotencijalnogMin>>minDoDolaska) {
+    if (minDoDolaska < Min){
+        brMin = brPotencijalnogMin;
+        Min = minDoDolaska;
+	}
   }
-
-  int taksista = 9999, vreme = 9999;
-  for (int j = 1; j <= i; j += 2) {
-    if (taksisti_vreme[j] < vreme) {
-      vreme = taksisti_vreme[j];
-      taksista = taksisti_vreme[j - 1];
-    }
-  }
-
-  cout << taksista << endl;
+  cout << brMin << endl;
   return 0;
 }
+
